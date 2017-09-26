57fc8ad56e096e09bcf29bc9e164e48466d3231a
Sorc278 <inbox@s278cloud.space>
2017-09-26 20:49:04 +0000

    Added quotes to home screen

--- home/models.py
+++ home/models.py
@@ -3,3 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Quote(models.Model):
+	quote = models.TextField(blank=False, null=False)
+	source = models.TextField(blank=False, null=False)
+	
+	def __str__(self):
+		return '{0!s} - {1!s}'.format(self.quote, self.source)
--- home/views.py
+++ home/views.py
@@ -1,12 +1,17 @@
+import random
+
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from storage.sizes import get_sizes_list_human
+from .models import Quote
 # Create your views here.
 
 @login_required
 def homepage(request):
-    return render(request, 'home/main.html', None)
+    return render(request, 'home/main.html', {
+        'quote': random.choice(Quote.objects.all())
+    })
 
 @login_required
 def profile(request):
