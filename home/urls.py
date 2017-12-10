6d4aec909748bed8a356f9bdc017ef01a5b3b78c
Sorc278 <inbox@s278cloud.space>
2017-12-10 23:06:14 +0000

    Bugfix

--- home/urls.py
+++ home/urls.py
@@ -7,5 +7,4 @@ app_name = 'home'
 urlpatterns = [
 	url(r'^$', views.homepage, name='homepage'),
 	url(r'^profile$', views.profile, name='profile'),
-	url(r'^get_CSRF$', views.get_CSRF, name='get_CSRF'),
 ]
